1ebcf5d7a501697ef623a9bbb41a10d2a31cf58b
luckysoni786 <luckysoni9782@.com>
2025-08-27 16:52:55 +0530

    second commit

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,11 +6,13 @@ import ProfilePage from './pages/ProfilePage'
 const App = () => {
   return (
     <>
+    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
     <Routes>
       <Route path='/' element={<HomePage/>} />
       <Route path='/login' element={<LoginPage/>} />
       <Route path='/profile' element={<ProfilePage/>} />
     </Routes>
+    </div>
     </>
   )
 }
